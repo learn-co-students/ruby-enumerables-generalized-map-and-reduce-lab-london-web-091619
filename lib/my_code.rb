e52ccf43f1d42d8c6b12e078b1f030f0cce20ba5
David Mynors <stuff@davidmyno.rs>
2019-07-31 17:40:21 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# Your Code Here
+def map(arr)
+  i = 0
+  result = []
+  while i < arr.length
+    result << yield(arr[i])
+    i += 1
+  end
+
+  result
+end
+
+def reduce(arr, initial = nil)
+  if initial
+    result = initial
+    i = 0
+  else
+    result = arr.first
+    i = 1
+  end
+  
+  while i < arr.length
+    result = yield([result, arr[i]])
+    i += 1
+  end
+  
+  result
+end
