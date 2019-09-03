e10b379460c0be0ca5c515605d594c65fa4e70a0
Tim Golding <tgolding55@hotmail.com>
2019-09-03 22:13:05 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(array)
+i=0
+newarr = []
+while i < array.length
+newarr[i] = yield(array[i])
+i+=1
+end
+ newarr
+end
+
+def reduce(array, starting_point=nil)
+  i=0
+  if starting_point
+    val = starting_point
+  else
+    val = array[0]
+    i=1
+  end
+while i<array.length
+  val = yield(val, array[i])
+i+=1
+end
+val
+end
