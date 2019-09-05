d21ab552930fd5638f4868d7754c9502ff16a9d0
Aniket B <aniket.b@outlook.com>
2019-09-05 05:29:51 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(array)
+  arr = []
+  i = 0
+  while i < array.length
+    arr.push(yield array[i])
+    i += 1
+  end
+  arr
+end
+
+def reduce(array, counter = 0)
+  if counter > 0
+    starting_point = counter
+    i = 0
+  else
+    starting_point = array[0]
+    i = 1
+  end
+  
+  while i < array.length
+    starting_point = yield(starting_point, array[i])
+    i += 1
+  end
+  starting_point
+end
